899c9f319ee065f80a839a18f810dfa61f4c530f
unknown <dinkarkumar2112@gmail.com>
2024-05-25 16:29:13 +0530

    under dev

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,6 +4,7 @@ import Carouselslider from "../Carousel/Carouselslider";
 import Steps from "../Steps/Steps";
 import About from "../About/About";
 import Topproducts from "../Pages/Topproducts/Topproducts";
+import Banner from "../Pages/Banner/Banner";
 // import Topproducts from "../Pages/Topproducts/Topproducts";
 
 const Home = () => {
@@ -15,6 +16,7 @@ const Home = () => {
       <Steps />
       <About />
       <Topproducts />
+      <Banner />
 
       <div className="mt-60"></div>
     </>
